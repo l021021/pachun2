171da43cd1bf8e1ab9b913b14f02f6dc3b012dd6
l021021 <653498331@qq.com>
2024-01-08 17:10:51 +0800

    before offset

--- dumpwebpage.py
+++ dumpwebpage.py
@@ -31,17 +31,49 @@ def flatten_json(nested_json, exclude=['']):
 
 
 # 定义URL
-url = "https://jobsapi-google.m-cloud.io/api/job/search?callback=jobsCallback&pageSize=1000&offset=0&companyName=companies%2Fd475e46c-52e0-4b9b-a525-48027b002583&customAttributeFilter=ats_portalid%3D%22SuccessFactors%22&orderBy=posting_publish_time%20desc&disableSpellCheck=true&enableBroadening=true"
+url = "https://jobsapi-google.m-cloud.io/api/job/search?callback=jobsCallback&pageSize=100&offset=0&companyName=companies%2Fd475e46c-52e0-4b9b-a525-48027b002583&customAttributeFilter=ats_portalid%3D%22SuccessFactors%22&orderBy=posting_publish_time%20desc&disableSpellCheck=true&enableBroadening=true"
+nextpagetoken = ''
 
 # 发送GET请求
 headers = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) \
-    AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.149 Safari/537.36'
+    AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.149 Safari/537.36',
+    'NextPageToken': nextpagetoken
 }
 response = requests.get(url, headers=headers)
+# 解释requests.get
+# requests.get(url, params=None, **kwargs)
+# url : URL for the new Request object.
+# params : (optional) Dictionary or bytes to be sent in the query string for the Request.
+# **kwargs : Optional arguments that request takes.
+# 返回的是一个response对象
+# response.text返回的是一个str对象
+# response.content返回的是一个bytes对象
+# response.json返回的是一个json对象
+
 
 # 从响应内容中删除"jobsCallback("和最后的")"，然后解析JSON数据
+
+
 data = json.loads(response.text[13:-1])
+
+nextpagetoken = data['nextPageToken']
+print(nextpagetoken)
+
+if nextpagetoken != '':
+    url = "https://jobsapi-google.m-cloud.io/api/job/search?callback=jobsCallback&pageSize=100&offset=100&companyName=companies%2Fd475e46c-52e0-4b9b-a525-48027b002583&customAttributeFilter=ats_portalid%3D%22SuccessFactors%22&orderBy=posting_publish_time%20desc&disableSpellCheck=true&enableBroadening=true"
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) \
+        AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.149 Safari/537.36',
+        'NextPageToken': nextpagetoken
+    }
+    response = requests.get(url, headers=headers)
+    data = json.loads(response.text[13:-1])
+
+data = json.loads(response.text[13:-1])
+
+nextpagetoken = data['nextPageToken']
+print(nextpagetoken)
 
 # 将数据写入本地JSON文件
 with open("page.json", "w", encoding='utf-8') as file:
